53cf172a4294a7d92903948aa73b0284b5a9e051
David Daw <daviddaw@gmail.com>
2025-01-24 21:33:40 -0800

    1/25/25 commentary

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import {Words} from './words.json'
 function App() {
 
-  const correctWord = ['E', 'V', 'E', 'N','T']
+  const correctWord = ['K', 'N', 'A', 'V','E']
   const focusRef = useRef(null);
 
   const [winner, setWinner] = useState(false)
